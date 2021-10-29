aba8f95d08cc96b403c07fe9a4a4cc133aa9f291
Irdy <need.more2017@yandex.ru>
2021-10-29 18:08:20 +0300

    cfg changes

--- build/lib/use_push_app/__init__.py
+++ build/lib/use_push_app/__init__.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-env_config = os.environ.get('APP_SETTINGS', 'config.DevelopmentConfig')
+env_config = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
 app.config.from_object(env_config)
 
 import use_push_app.controllers.index_controller
--- config.py
+++ config.py
@@ -1,17 +1,28 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     DEBUG: False
-    DEVELOPMENT: False
+    TESTING = False
+    CSRF_ENABLED = True
     # SECRET_KEY: os.getenv("SECRET_KEY", "this-is-the-default-key")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///use_push_dev')
 
 
 class ProductionConfig:
-    pass
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
 
 
 class DevelopmentConfig:
     DEBUG: True
     DEVELOPMENT: True
 
+
+class TestingConfig(Config):
+    TESTING = True
